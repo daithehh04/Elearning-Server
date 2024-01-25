c4682fe6a888838ec14e3bff10d1b62d468b1738
xuananh2212 <xuantuananh2212@gmail.com>
2024-01-26 02:40:23 +0700

    add api feedback

--- src/api/feedback.ts
+++ src/api/feedback.ts
@@ -1,45 +1,72 @@
-import express from 'express';
-import FeedbackService from '../services/feedback';
-import asyncHandler from '../utils/async_handle';
-import ENDPONTAPI from '../submodule/common/endpoint';
-import TTCSconfig from '../submodule/common/config';
+import express from "express";
+import FeedbackService from "../services/feedback";
+import asyncHandler from "../utils/async_handle";
+import ENDPONTAPI from "../submodule/common/endpoint";
+import TTCSconfig from "../submodule/common/config";
 
 const feedbackRouter = express.Router();
 const feedbackService = new FeedbackService();
 
-feedbackRouter.get(ENDPONTAPI.GET_FEEDBACKS, asyncHandler(async (req, res) => {
-    const {data, count} = await feedbackService.getFeedbacks()
+feedbackRouter.get(
+  ENDPONTAPI.GET_FEEDBACKS,
+  asyncHandler(async (req, res) => {
+    const { data, count } = await feedbackService.getFeedbacks();
     return res.json({
-        data,
-        count,
-        status : TTCSconfig.STATUS_SUCCESS
-    })
-}))
+      data,
+      count,
+      status: TTCSconfig.STATUS_SUCCESS,
+    });
+  })
+);
 
-feedbackRouter.post(ENDPONTAPI.GET_FEEDBACKS_BY_COURSE, asyncHandler(async (req, res) => {
-    const {data, count} = await feedbackService.getFeedbacksByCourse({idCourse: `${req.query.idCourse}`})
+feedbackRouter.post(
+  ENDPONTAPI.GET_FEEDBACKS_BY_COURSE,
+  asyncHandler(async (req, res) => {
+    const { data, count } = await feedbackService.getFeedbacksByCourse({
+      idCourse: `${req.query.idCourse}`,
+    });
     return res.json({
-        data,
-        count,
-        status : TTCSconfig.STATUS_SUCCESS
-    })
-}))
+      data,
+      count,
+      status: TTCSconfig.STATUS_SUCCESS,
+    });
+  })
+);
 
-feedbackRouter.post(ENDPONTAPI.GET_FEEDBACKS_BY_TYPE_OR_COURSE, asyncHandler(async (req, res) => {
-    const {data, count} = await feedbackService.getFeedbacksByTypeOrCourse({type: req.query.type as string[], idCourse: `${req.query.idCourse}`})
+feedbackRouter.post(
+  ENDPONTAPI.GET_FEEDBACKS_BY_TYPE_OR_COURSE,
+  asyncHandler(async (req, res) => {
+    const { data, count } = await feedbackService.getFeedbacksByTypeOrCourse({
+      type: req.query.type as string[],
+      idCourse: `${req.query.idCourse}`,
+    });
     return res.json({
-        data,
-        count,
-        status : TTCSconfig.STATUS_SUCCESS
-    })
-}))
+      data,
+      count,
+      status: TTCSconfig.STATUS_SUCCESS,
+    });
+  })
+);
 
-feedbackRouter.post(ENDPONTAPI.CREATE_FEEDBACK, asyncHandler(async (req, res) => {
-    const data = await feedbackService.createFeedback(req.body)
+feedbackRouter.post(
+  ENDPONTAPI.CREATE_FEEDBACK,
+  asyncHandler(async (req, res) => {
+    const data = await feedbackService.createFeedback(req.body);
     return res.json({
-        data,
-        status : TTCSconfig.STATUS_SUCCESS
-    })
-}))
+      data,
+      status: TTCSconfig.STATUS_SUCCESS,
+    });
+  })
+);
+feedbackRouter.post(
+  ENDPONTAPI.DELETE_FEEDBACK,
+  asyncHandler(async (req, res) => {
+    const { data, count } = await feedbackService.deleteFeedback(req.body);
+    return res.json({
+      data,
+      status: TTCSconfig.STATUS_SUCCESS,
+    });
+  })
+);
 
-export { feedbackRouter }
+export { feedbackRouter };
